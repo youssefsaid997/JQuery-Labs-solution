b870b1170d29c7587e3f5bb9ac8cdd991c156844
youssefsaid997 <57658169+youssefsaid997@users.noreply.github.com>
2023-12-28 13:20:44 +0200

    hotfix : fixing active behaviour and adding right click to hide

--- tabs/script.js
+++ tabs/script.js
@@ -3,10 +3,19 @@
 // 	$(this).addClass('active');
 // });
 
+// $('.main').css({ display: 'none' });
+
 $('.btn').each(function (index, button) {
 	$(button).click(function () {
+		$('.main').css({ display: 'block' });
 		$('.btn').removeClass('active');
 		$(this).addClass('active');
 		$('.main').html('<div class=content>Content ' + (index + 1) + '</div>');
+	});
+
+	//this event will handle the right click to hide the content section
+	$(button).on('contextmenu', function (event) {
+		event.preventDefault();
+		$('.main').css({ display: 'none' });
 	});
 });
